2b3c388afa932b54da6c61b283db8b8517fc7db9
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-06-10 22:40:55 +0530

    Add ImageKit and Axios integration with authentication in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import axios from "axios";
+import { ImageKitProvider } from "imagekitio-next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,18 +20,37 @@ export const metadata: Metadata = {
   description: "Store it. Share it. Boxit.",
 };
 
+const authenticator = async () => {
+    try{
+      const res = await axios.get("/api/imagekit-auth");
+      return res.data;
+    }catch(error){
+      console.error("Authentication error:", error);
+      throw error;
+    }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>)
+{
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <ImageKitProvider
+        publicKey={process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string}
+        urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT as string}
+        authenticator={ authenticator }
       >
-        {children}
-      </body>
+        <ClerkProvider>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+            {children}
+          </body>
+        </ClerkProvider>
+      </ImageKitProvider>
     </html>
   );
 }
